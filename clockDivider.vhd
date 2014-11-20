6065e2bcacf8551dd6e1d82ad6d7f1ea229c71fa
Michael Nickelson <michael.nickelson@gmail.com>
2014-11-19 23:29:29 -0500

    Updated files from current checkin of vgaController

--- clockDivider.vhd
+++ clockDivider.vhd
@@ -1,28 +1,29 @@
 LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.all;
-USE IEEE.std_logic_unsigned.all;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+
+-- This is a basic clock divider that takes in a 50MHz clock and outputs a
+-- 25MHz clock. It is used as an enable for the hSync and vSync counters.
 
 entity clockDivider is
-   port
-	(
-      clk50_I  :  in std_logic;
-      reset_I  :  in std_logic;
-      clk25_O  :  out std_logic
-	);
+  port(
+    clk50_I : in  std_logic;
+    reset_I : in  std_logic;
+    clk25_O : out std_logic);
 end entity;
 
 architecture rtl of clockDivider is
-   signal i_clk : std_logic;
-   
-   begin
-      clk25_O <= i_clk;
+  signal i_clk : std_logic;
 
-   process (clk50_I, reset_I)
-	begin
-      if reset_I = '1' then
-         i_clk <= '0';
-		elsif (rising_edge(clk50_I)) then
-         i_clk <= NOT i_clk;
-      end if;
-	end process;
+begin
+  clk25_O <= i_clk;
+
+  process(clk50_I, reset_I)
+  begin
+    if reset_I = '1' then
+      i_clk <= '0';
+    elsif (rising_edge(clk50_I)) then
+      i_clk <= NOT i_clk;
+    end if;
+  end process;
 end architecture rtl;
--- counter.vhd
+++ counter.vhd
@@ -1,50 +1,66 @@
--- Quartus II VHDL Template
--- Binary Counter
-
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use ieee.std_logic_unsigned.all;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+USE IEEE.NUMERIC_STD.ALL;
 
 entity counter is
-	generic
-	(maxCount : integer := 3);
-    -- pulseWidth : integer := 0,
-    -- backPorch : integer := 3,
-    -- blankSection : integer := 3,
-    -- displayOn : integer := 3,
-    -- frontPorch : integer := 3);
-
-	port
-	(
-		clk_I		:  in std_logic;
-		reset_I	:  in std_logic;
-		enable_I	:  in std_logic;
-		count_O	: out std_logic_vector (9 DOWNTO 0)
-	);
+  generic(
+-- The various timing sections of a cycle are all included as generics
+-- backPorch is not currently used but is included for completeness
+    maxCount     : std_logic_vector(9 DOWNTO 0) := (OTHERS => '0');
+    pulseWidth   : std_logic_vector(9 DOWNTO 0) := (OTHERS => '0');
+    backPorch    : std_logic_vector(9 DOWNTO 0) := (OTHERS => '0');
+    blankSection : std_logic_vector(9 DOWNTO 0) := (OTHERS => '0');
+    displayOn    : std_logic_vector(9 DOWNTO 0) := (OTHERS => '0');
+    frontPorch   : std_logic_vector(9 DOWNTO 0) := (OTHERS => '0'));
+  port(
+    clk_I      : in  std_logic;
+    reset_I    : in  std_logic;
+    enable_I   : in  std_logic;
+  -- videoOn_O is 1 during the "active" portion of a scan and 0 during front
+  -- porch/back porch/etc
+    videoOn_O  : out std_logic;
+  -- carry_O is high during the last cycle before the sync pulse and is used as
+  -- part of the enable input from hSync to vSync
+    carry_O    : out std_logic;
+  -- pulseL_O is the active low sync pulse sent to a VGA device
+    pulseL_O   : out std_logic;
+    count_O    : out std_logic_vector(9 DOWNTO 0));
 end entity;
 
 architecture rtl of counter is
-signal i_count    : std_logic_vector (9 DOWNTO 0);
-signal i_maxCount : std_logic_vector (9 DOWNTO 0);
-
+  signal i_count       : std_logic_vector(9 DOWNTO 0);
+  signal i_displayOn   : std_logic_vector(9 DOWNTO 0);
+-- i_startOfSync and i_endOfSync are the counter values when the sync pulse begins and ends
+  signal i_startOfSync : std_logic_vector(9 DOWNTO 0);
+  signal i_endOfSync   : std_logic_vector(9 DOWNTO 0);
 
 begin
-	i_maxCount <= std_logic_vector(to_unsigned(maxCount,i_maxCount'length));
-	count_O <= i_count;
-	
-   process (clk_I, reset_I)
-	begin
-      if reset_I = '1' then
-         i_count <= (OTHERS => '0');
-		elsif (rising_edge(clk_I)) then
-			if enable_I = '1' then	   
-				i_count <= i_count + 1;
-				if (i_count = i_maxCount) then
-					i_count <= (OTHERS => '0');
-				end if;
-			end if;
-		end if;
-	end process;
+  i_displayOn   <= displayOn - 1;
+  i_startOfSync <= i_displayOn + blankSection + frontPorch;
+  i_endOfSync   <= i_displayOn + blankSection + frontPorch + pulseWidth;
 
+  count_O <= i_count;
+
+  videoOn_O <= '1' WHEN (i_count <= i_displayOn) ELSE '0';
+  carry_O   <= '1' WHEN (i_count = i_displayOn + blankSection + frontPorch) ELSE '0';
+  pulseL_O  <= '0' WHEN ((i_count > i_startOfSync) AND (i_count <= i_endOfSync)) ELSE '1';
+
+-- The count process is only sensitive to clk_I and reset_I. It increments by
+-- one every other clock cycle due to the clock divided enable and resets at
+-- maxCount - 1.
+  process(clk_I, reset_I)
+  begin
+    if reset_I = '1' then
+      i_count <= (OTHERS => '0');
+    elsif rising_edge(clk_I) then
+      if (enable_I = '1') then
+        if (i_count < (maxCount - 1)) then
+          i_count <= i_count + 1;
+        else
+          i_count <= (OTHERS => '0');
+        end if;
+      end if;
+    end if;
+  end process;
 end rtl;
